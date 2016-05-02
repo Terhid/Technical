26400710228c9b45d5947751d55b70ff2713015d
nortonajec <muotie@gmail.com>
2016-05-02 16:20:14 +0200

    Added simple_ema and fixed PEP 8 mistakes

--- indicator_functions.py
+++ indicator_functions.py
@@ -1,6 +1,21 @@
 import data_processing as dp
 
 
+def simple_ema(n, a, data):
+    """Returns EMA(N) for a days back"""
+    i = 0
+    numerator = 0
+    denominator = 0.00000001
+    n = int(n)
+    alpha = 2.0 / (n + 1)
+    for row in data[a - 1:n + a - 1]:
+        numerator += (1 - alpha) ** i * row[4]
+        denominator += (1 - alpha) ** i
+        i += 1
+    ema = numerator / denominator
+    return ema
+
+
 def ema(n, b1, b2, list):
     """Returns EMA(N) of a list for a given [b1,b2] interval"""
     emalist = []
