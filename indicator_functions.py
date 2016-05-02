591f9345d33dafda4e6a2ec3976823cbf8e1fd17
nortonajec <muotie@gmail.com>
2016-05-02 16:25:08 +0200

    Small improvements for variable names

--- indicator_functions.py
+++ indicator_functions.py
@@ -1,14 +1,14 @@
 import data_processing as dp
 
 
-def simple_ema(n, a, data):
-    """Returns EMA(N) for a days back"""
+def simple_ema(n, b, data):
+    """Returns ema(n) for b days back"""
     i = 0
     numerator = 0
     denominator = 0.00000001
     n = int(n)
     alpha = 2.0 / (n + 1)
-    for row in data[a - 1:n + a - 1]:
+    for row in data[b - 1:n + b - 1]:
         numerator += (1 - alpha) ** i * row[4]
         denominator += (1 - alpha) ** i
         i += 1
@@ -26,8 +26,8 @@ def ema(n, b1, b2, list):
     for i in range(0, b2 - b1):
         numerator += (1 - alpha) ** i * list[i + b1]
         denominator += (1 - alpha) ** i
-        EMA = numerator / denominator
-        emalist.append(EMA)
+        ema = numerator / denominator
+        emalist.append(ema)
 
     emalist = emalist[::-1]
     return emalist
@@ -41,8 +41,7 @@ def testing():
         datalist.append(data[i][4])
     dane_nieodwr = datalist[::-1]
     emalist = ema(50, 1, 50, dane_nieodwr)
-    print (emalist)
+    print emalist
 
 
 testing()
-
