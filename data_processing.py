e4d45b7b377f81abcfcf3d62c5d35d2e1863b841
nortonajec <muotie@gmail.com>
2016-05-06 00:55:48 +0200

    Added extracting functions for opening prices, volume, min prices, max prices

--- data_processing.py
+++ data_processing.py
@@ -30,6 +30,19 @@ def data_processing(downloaded_data):
     return processed_data
 
 
+def opening_prices(processed_data):
+    """
+    Extracts opening prices from processed data
+
+    :param processed_data: data without column names processed by data_processing() function
+    :return: list of opening prices
+    """
+    opening_prices_list = []
+    for i in range(len(processed_data)):
+        opening_prices_list.append(processed_data[i][1])
+    return opening_prices_list
+
+
 def closing_prices(processed_data):
     """
     Extracts closing prices from processed data
@@ -41,3 +54,42 @@ def closing_prices(processed_data):
     for i in range(len(processed_data)):
         closing_prices_list.append(processed_data[i][4])
     return closing_prices_list
+
+
+def max_prices(processed_data):
+    """
+    Extracts max prices from processed data
+
+    :param processed_data: data without column names processed by data_processing() function
+    :return: list of max prices
+    """
+    max_prices_list = []
+    for i in range(len(processed_data)):
+        max_prices_list.append(processed_data[i][2])
+    return max_prices_list
+
+
+def min_prices(processed_data):
+    """
+    Extracts min prices from processed data
+
+    :param processed_data: data without column names processed by data_processing() function
+    :return: list of min prices
+    """
+    min_prices_list = []
+    for i in range(len(processed_data)):
+        min_prices_list.append(processed_data[i][3])
+    return min_prices_list
+
+
+def volume(processed_data):
+    """
+    Extracts volume values from processed data
+
+    :param processed_data: data without column names processed by data_processing() function
+    :return: list of volume values
+    """
+    volume_list = []
+    for i in range(len(processed_data)):
+        volume_list.append(processed_data[i][5])
+    return volume_list
